9f3dba70589a12a40dc5c427ee4a1ec2c1d419d1
jamst <jamstjamst@163.com>
2018-09-21 22:54:58 +0800

    默认路由

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,5 +1,5 @@
 class VendorsController < ApplicationController
-  before_action :authenticate_user!, except: [:index,:show,:review,:details]
+  #before_action :authenticate_user!, except: [:index,:show,:review,:details]
   before_action :set_vendor, only: [:show, :edit, :update, :destroy,:add_comments]
   layout "article"
   require 'exifr/jpeg'
