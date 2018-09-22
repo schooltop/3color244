dbcb32bbcea171f2fc2b43166496ac94d9474763
jamst <jamstjamst@163.com>
2018-09-22 08:24:16 +0800

    作品展示

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -96,11 +96,11 @@ class ArticlesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article
-      @article = Article.find(params[:id])
+      @article = Article.find_by(id:params[:id]) || Article.find_by(id:1)
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:category_id,:title,:content,:cover_img)
+      params.require(:article).permit(:category_id,:title,:content,:cover_img,:min_show,:user_id,:vendor_id)
     end
 end
