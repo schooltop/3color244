cb45d9682f6e96f45e3bdcd0299791aa7cff313c
jamst <jamstjamst@163.com>
2018-09-26 14:42:08 +0800

    floot描述

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
       time.localtime.to_s(:db)
     end
   end
+
+  def qq_link(qq)
+    return "" if qq.blank?
+    link_to "tencent://message/?Menu=yes&uin=#{qq}", data: {content: "QQ联系: #{qq}"}, class: 'popups' do
+      "<i class='am-icon-qq'></i>".html_safe
+    end
+  end
+
 end
